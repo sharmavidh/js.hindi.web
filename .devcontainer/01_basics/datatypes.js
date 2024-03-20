58ef3a87f592efa929fb14fd3b77511639e6ba32
sharmavidh <163817847+sharmavidh@users.noreply.github.com>
2024-03-20 15:08:32 +0000

    datatypes done

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict";//treat all js code as  newer version
+
+//alert( 3 + 3 ) //we are using node.js,not browser
+
+console.log (3+3)
+console.log ("vidhi")
+
+let name ="vidhi"
+let age = 23
+let isLoggedIn =false
+let state;
+
+//number => 2 to power 53
+//bigint
+//string =>
+//boolean =>true/false
+//null => standalone value/object
+//undefined =>
+//symbol
+//object
+
+console.log(typeof "vidhi");
