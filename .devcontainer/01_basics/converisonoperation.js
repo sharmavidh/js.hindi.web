817356f4e7623a0aca08f2738abb21e0fd6a017b
sharmavidh <163817847+sharmavidh@users.noreply.github.com>
2024-03-21 07:47:56 +0000

    conversion

--- .devcontainer/01_basics/converisonoperation.js
+++ .devcontainer/01_basics/converisonoperation.js
@@ -1,11 +1,11 @@
-let score = undefined
+/*let score = undefined
 
 console.log(typeof score);
 
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
-console.log(valueInNumber);
+console.log(valueInNumber);*/
 
 
 //"33"easily conert in 33
@@ -19,15 +19,39 @@ console.log(valueInNumber);
 //console.log(booleanIsLoggedIn);
 
 
-let isLoggedIn ="vidhi"
+/*let isLoggedIn ="vidhi"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);*/
 
 
 
-let someNumber = 22
+//let someNumber = 22
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+//let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber)
+
+
+
+
+
+//*****************************************Operations******************************************************************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = "Nannu"
+
+let str3 = str1 + str2
+console.log(str3)
