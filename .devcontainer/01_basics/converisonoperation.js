f964c732b32a6a8e83f5ed894718ca05e49b62bc
sharmavidh <163817847+sharmavidh@users.noreply.github.com>
2024-03-21 05:18:41 +0000

    coversion of datatypes

--- .devcontainer/01_basics/converisonoperation.js
+++ .devcontainer/01_basics/converisonoperation.js
@@ -1,4 +1,33 @@
-let score = 33
+let score = undefined
+
 console.log(typeof score);
 
 
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33"easily conert in 33
+//"33 abc"conert in NaN
+//TRUE convert in 1
+//False convert in 0
+//"vidhi" convert in true
+//let isLoggedIn = 1
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+let isLoggedIn ="vidhi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
