5fe811f79e9c8d4292810aba20df3d246020fafc
sharmavidh <163817847+sharmavidh@users.noreply.github.com>
2024-03-22 04:22:43 +0000

    changes

---
+++ .devcontainer/01_basics/datatypesnotes.js
@@ -0,0 +1,37 @@
+//primitive data type
+//7 types:string, number, boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+//const bigNumber = 15156854526526529n
+
+
+
+
+//non-primitive 0r reference data type
+//arrays , objects ,functions,
+
+// const  heros = {"shaktiman", "naagraj","doga"};
+// let myObj = {
+//     name:"hitesh"
+//     age: 22,
+
+//
+
+const myFunction = function(){
+    console.log("Hello vidhi")
+}
+
+console.log(typeof anotherId);
+// return data type
+//null= object
