a3cad174fe3d5bca931e60dbb0be153832101e94
sharmavidh <163817847+sharmavidh@users.noreply.github.com>
2024-03-22 02:53:48 +0000

    changes

--- .devcontainer/01_basics/converisonoperation.js
+++ .devcontainer/01_basics/converisonoperation.js
@@ -51,7 +51,14 @@ let negValue = -value
 
 
 let str1 = "hello"
-let str2 = "Nannu"
+let str2 = " vidhi"
 
 let str3 = str1 + str2
-console.log(str3)
+console.log(str3);
+
+console.log("1" +2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
