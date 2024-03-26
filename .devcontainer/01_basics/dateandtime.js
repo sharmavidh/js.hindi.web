940a361878defb216d76be656b9f16bb8e1ea78c
sharmavidh <163817847+sharmavidh@users.noreply.github.com>
2024-03-26 04:18:35 +0000

    done change

--- .devcontainer/01_basics/dateandtime.js
+++ .devcontainer/01_basics/dateandtime.js
@@ -22,3 +22,32 @@ let myTimeStamp = Date.now()
 //console.log(myTimeStamp);
 //console.log(myCreatedDate.getTime());
 //console.log(Math.floor(Date.now()/1000));
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+//$(newDate.getDay())
+//and the time
+
+
+newDate.toLocaleString('default',{
+    weekday: "long",
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+)
