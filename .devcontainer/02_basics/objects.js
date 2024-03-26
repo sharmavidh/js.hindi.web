7a6a329cb88671bd329132ae7612042c94fda75d
sharmavidh <163817847+sharmavidh@users.noreply.github.com>
2024-03-26 15:55:13 +0000

    change done

---
+++ .devcontainer/02_basics/objects.js
@@ -0,0 +1,16 @@
+//singleton
+//object.create
+//object literals
+
+const Jsuser = {
+    name: "Vidhi",
+    age: 23,
+    location: "Jaipur",
+    email: "vs@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
