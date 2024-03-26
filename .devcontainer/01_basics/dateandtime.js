4d589296fd8ded8a1a81880416c7e776a599d0e6
sharmavidh <163817847+sharmavidh@users.noreply.github.com>
2024-03-26 04:04:27 +0000

    change

--- .devcontainer/01_basics/dateandtime.js
+++ .devcontainer/01_basics/dateandtime.js
@@ -21,4 +21,4 @@ let myTimeStamp = Date.now()
 
 //console.log(myTimeStamp);
 //console.log(myCreatedDate.getTime());
-console.log(Math.floor(Date.now()/1000));
+//console.log(Math.floor(Date.now()/1000));
