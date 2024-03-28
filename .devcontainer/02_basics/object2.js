d1473de4ba1b4d005552114057112f2c7f0f425b
sharmavidh <163817847+sharmavidh@users.noreply.github.com>
2024-03-28 08:52:10 +0000

    object change

--- .devcontainer/02_basics/object2.js
+++ .devcontainer/02_basics/object2.js
@@ -1,3 +1,18 @@
-const tinderUser = new Object()
+//const tinderUser = new Object()
+const tinderUser = {}
 
-console.log(tinderUser);
+tinderUser.id = "123"
+tinderUser.name = "vidhi"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+const regularUser = {
+    email: "vs@gmail.com",
+    fullname : {
+        userfullname : {
+            firstname : "vidhi",
+            lastname : "sharma"
+        }
+    }
+}
