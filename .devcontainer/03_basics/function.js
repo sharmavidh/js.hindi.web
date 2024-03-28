8a4d8bbd0bf71c8fbdce9b1731d8aee7eea2a8bf
sharmavidh <163817847+sharmavidh@users.noreply.github.com>
2024-03-28 11:58:33 +0000

    function changs

---
+++ .devcontainer/03_basics/function.js
@@ -0,0 +1,36 @@
+function sayMyName(){
+console.log("v");
+console.log("i");
+console.log("d");
+console.log("h");
+console.log("i");
+}
+
+//sayMyName()
+
+// function addTwoNumbers(number1, number2) {
+//     console.log(number1 + number2);
+    
+// }
+//addTwoNumbers(3 ,"4")
+//addTwoNumbers(3 , null)
+function addTwoNumbers(number1, number2) {
+let result = number1 + number2
+//console.log("vidhi");
+return result
+//console.log("vidhi");
+    
+}
+const result = addTwoNumbers( 3 , 5)
+//console.log("Result:", result);
+
+function loginUserMessage(username= "vs") {
+    if (!username) {
+        console.log("Please enter a username");
+        return
+    }
+    return '${username} just logged in'
+    
+}
+//console.log(loginUserMessage("vidhi"))
+console.log(loginUserMessage);
