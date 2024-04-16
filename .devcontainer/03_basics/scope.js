bae037ca7f84ced38c1a21381401f1c96ccb71ea
sharmavidh <163817847+sharmavidh@users.noreply.github.com>
2024-04-16 11:24:04 +0000

    scope done

---
+++ .devcontainer/03_basics/scope.js
@@ -0,0 +1,17 @@
+ //two types - block , global scope
+ 
+ let a = 300
+if (true) {
+let a = 10
+const b = 20
+var c = 30
+console.log("INNER:", a);
+}
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+//console.log(a);
+//console.log(b);
+//console.log(c);
