00da3f9b2b840eb55994cb893a4b4d5b3db07e52
sharmavidh <163817847+sharmavidh@users.noreply.github.com>
2024-04-23 08:06:19 +0000

    arrow changes

---
+++ .devcontainer/03_basics/arrow.js
@@ -0,0 +1,52 @@
+const user = {
+    username: "vs",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "a"
+// user.welcomeMessage()
+
+//console.log(this);
+
+// function chai() {
+//     let username = "vs"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "vs"
+//      console.log(this.username);
+// }
+
+// const chai =  () => {
+//     let username = "vs"
+//      console.log(this);
+// }
+
+// chai()
+
+
+// const addtwo = (num1, num2) =>{
+//     return num1 + num2
+// }
+
+// console.log(addtwo(3, 4));
+    
+//const addtwo = (num1, num2) => (num1 + num2)
+// const addtwo = (num1, num2) => {username: "vs"}
+
+// console.log(addtwo(3, 4));
+
+const myArray = [2, 3, 5, 6]
+
+myArray.forEach()
+
+
