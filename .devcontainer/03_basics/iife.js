f8f4ad91bd3d7ce8e2741c437d00f8c86fb5d1a0
sharmavidh <163817847+sharmavidh@users.noreply.github.com>
2024-04-25 05:49:06 +0000

    COMMIT

--- .devcontainer/03_basics/iife.js
+++ .devcontainer/03_basics/iife.js
@@ -4,7 +4,7 @@
     //named IIFE
     console.log(`DB CONNECTED`);
 
-})();
+})();//NOTE YADI 2 IIFE EK SATH LIKHNE H TO FIRST KE BAD SEMICOLON LGANA JARURI H NHI TO 2 RUN NHI KREGA
 //unnamed IIFE
 ( (name) => {
     console.log(`DB CONNECTED 2 ${name}`);
