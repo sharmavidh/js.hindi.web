bb836a7ea03f5ab8b086a7c561138bed2d90198f
sharmavidh <163817847+sharmavidh@users.noreply.github.com>
2024-04-25 06:51:59 +0000

    one push

--- .devcontainer/04_controlflow/one.js
+++ .devcontainer/04_controlflow/one.js
@@ -3,53 +3,57 @@
 // if (isUserLoggedIn) {
     
 // }
-// 2<=2
-// <, >, <=, >=, ==, !=
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const UserLoggedIn = true
-const debitCard = true
-const loggedInFromGoogle = false
-const loggedInFromEmail = true
-
-if (UserLoggedIn && debitCard && 2==3 ) {
-    console.log("Allow to buy course");
+// 3 !=2
+// <, >, <=, >=, ==, !=, ===
+if (2 === "2") {
+    console.log("executed");
     
 }
-if (loggedInFromGoogle || loggedInFromEmail) {
-    console.log("User logged in");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const UserLoggedIn = true
+// const debitCard = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true
+
+// if (UserLoggedIn && debitCard && 2==3 ) {
+//     console.log("Allow to buy course");
     
-}
+// }
+// if (loggedInFromGoogle || loggedInFromEmail) {
+//     console.log("User logged in");
+    
+// }
