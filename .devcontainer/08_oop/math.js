8fb541195f3f326833446a14b5764916473f51f1
sharmavidh <163817847+sharmavidh@users.noreply.github.com>
2024-05-26 08:49:57 +0000

    code push

--- .devcontainer/08_oop/math.js
+++ .devcontainer/08_oop/math.js
@@ -1,7 +1,35 @@
-const descriptor = Object.getOwnPropertyDescriptor(Math, "PI")
+// const descriptor = Object.getOwnPropertyDescriptor(Math, "PI")
 
 
-console.log(descriptor);
-// console.log(Math.PI);
-// Math.PI = 5
-// console.log(Math.PI);
+// console.log(descriptor);
+// // console.log(Math.PI);
+// // Math.PI = 5
+// // console.log(Math.PI);
+
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true,
+
+    orderchai: function(){
+        console.log("chai nhi bani");
+        
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai , "name"));
+
+ Object.defineProperty(chai, 'name', {
+    //writable: false,
+    enumerable: true,
+    //iteratable: false
+ })
+ console.log(Object.getOwnPropertyDescriptor(chai , "name"));
+
+
+for (let [key, value] of Object.entries(chai)){
+    if(typeof value !== 'function') {
+
+    }
+    console.log(`${key} : ${value}`);
+}
